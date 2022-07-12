f59834151725e519da5c3f44d5493c66e535c9a5
Mr Solo Dev <95209979+mrsolodev450@users.noreply.github.com>
2022-07-12 13:55:01 +0530

    Add files via upload

---
+++ status/status.js
@@ -0,0 +1,41 @@
+var contacts = [{
+  name : 'Tom',
+  phone : 1234567890,
+  pic : '/images/client1.png'
+},
+{
+  name : 'Jone',
+  phone : 0987654321,
+  pic : '/images/client2.png'
+}]
+
+for(let i = 0; i < contacts.length; i++){
+  addChat(contacts[i].name, contacts[i].phone, contacts[i].pic)
+}
+
+function addChat(name, phone, pic) {
+  const TEM = document.createElement('div')
+  const NAME = document.createElement('strong')
+  const PIC = document.createElement('img')
+  const I = document.createElement('i')
+  
+  
+  NAME.append(name)
+  PIC.append(pic)
+  
+  TEM.classList.add('user-tem')
+  
+  NAME.classList.add('name')
+  
+  I.setAttribute('class', 'bi bi-three-dots-vertical')
+  
+  PIC.src = pic
+  PIC.classList.add('display-pic')
+  
+  TEM.append(PIC, NAME)
+  
+  document.body.append(TEM)
+  
+}
+
+
